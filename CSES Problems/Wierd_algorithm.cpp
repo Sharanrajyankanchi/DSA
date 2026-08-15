a71feaa6e439c9e71b2f02384554fc420e620965
Sharanraj Yankanchi <yankanchisharanraj658@gmail.com>
2026-08-15 10:11:19 +0530

    Add files via upload

---
+++ CSES Problems/Wierd_algorithm.cpp
@@ -0,0 +1,27 @@
+/*
+                link    : https://cses.fi/problemset/task/1068/
+ */
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n;
+    cin >> n;
+    cout << n << " ";
+    while (n != 1)
+    {
+        if (n % 2 == 0)
+        {
+            n /= 2;
+        }
+        else
+        {
+            n = n * 3 + 1;
+        }
+        cout << n << " ";
+    }
+    return 0;
+}
